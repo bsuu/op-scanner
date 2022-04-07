6843508a20c6e7b4100cb730f4316450ff153f2f
Szymon Góralczyk <bezszy@gmail.com>
2022-04-07 14:10:09 +0200

    navbar wowow sie rusza

--- lib/main_widget.dart
+++ lib/main_widget.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:skan/pages/camera_view.dart';
 import 'package:skan/pages/file_view.dart';
 import 'package:skan/pages/profile_view.dart';
-import 'package:skan/widgets/navbar.dart';
+import 'package:skan/widgets/navbar_widget.dart';
 
 import 'octicons_icons.dart';
 
@@ -22,8 +22,6 @@ class MainWidgetState extends State<MainWidget> {
   int selectedIndex = 0;
 
   void changeIndexState(int index) {
-    print("tapped ");
-    print(index);
     setState(() {
       selectedIndex = index;
     });
@@ -36,13 +34,14 @@ class MainWidgetState extends State<MainWidget> {
         textDirection: TextDirection.ltr,
           child: Scaffold(
             body: PageView(
-
               controller: _controller,
               children: [
                 FileView(),
                 CameraView(),
                 ProfileView(),
               ],
+                onPageChanged: changeIndexState,
+
             ),
             bottomNavigationBar: Navbar(
                 controller: _controller,
---
+++ lib/widgets/navbar_widget.dart
@@ -0,0 +1,75 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NavbarState extends State<Navbar> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double size = ((MediaQuery.of(context).size.width - 96) / 3);
+    return Container(
+      constraints: BoxConstraints(
+        maxHeight: 46
+      ),
+      margin: EdgeInsets.only(bottom: 14, left: 48, right: 48),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+        color: Colors.yellow,
+      ),
+      child: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            margin: EdgeInsets.only(left: size * widget.currentIndex),
+            width: size,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              color: Colors.red,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 12, bottom: 12),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                for (int i = 0; i < widget.items.length; i++)
+                  GestureDetector(
+                    onTap: () => widget.onTap(i),
+                    child: widget.items[i],
+                  )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+}
+
+class Navbar extends StatefulWidget {
+
+  final PageController controller;
+  final List<Widget> items;
+  final int currentIndex;
+  var onTap;
+
+  Navbar({Key? key,
+    required this.controller,
+    required this.items,
+    required this.currentIndex,
+    this.onTap
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => NavbarState();
+}
